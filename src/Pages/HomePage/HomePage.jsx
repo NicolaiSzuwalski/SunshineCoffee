241becbc1a11bcc833a2919f835cabd7282074fe
NicolaiSzuwalski <132432642+NicolaiSzuwalski@users.noreply.github.com>
2024-09-13 01:10:54 +0200

    goToProducts added

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Navbar } from '../../components/Navbar/Navbar.jsx'
 import { Header } from '../../components/Header/Header.jsx'
+import { NavLink } from 'react-router-dom'
+import styles from './HomePage.module.scss'
 
 export const HomePage = () => {
   return (
     <>
     <Header></Header>
+    <div className={styles.ShopNow}>
+      <h2>Shop now</h2>
+      <p>Our delicious coffees wil get you brewing the best cup of coffee you ever tasted in no time at all. And the best part of it? It is totally organic, fair trade and sustainably sourced. So get brewing</p>
+      <NavLink to='/Products'><button>Go to Products</button></NavLink>
+    </div>
     </>
   )
 }
